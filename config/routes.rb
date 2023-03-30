2c8fab5ea32f9ef20dc8a42bf685f62412b504a8
Collins Chirchir <collinskchirchir@gmail.com>
2023-03-30 21:39:29 +0300

    user stays logged in

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
 
   post '/login', to: 'sessions#create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #stay loggged in
+  get 'me', to: 'users#show'
+  
 end
